7ea45e31108b9042adf1e29a3e08e432b39ea68b
Charlie Hield <charlie.hield@collemcvoy.com>
2019-02-12 15:05:58 -0600

    Add basic Nuxt front-end and monorepo setup

---
+++ www/components/GameCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>{{ game.date }}</p>
+    <p>{{ game.time }}</p>
+    <p>{{ game.rink }}</p>
+    <p>{{ game.home }}</p>
+    <p>{{ game.away }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GameCard',
+
+  props: ['game']
+}
+</script>
+
---
+++ www/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <game-card v-for="(game, i) in games" :key="i" :game="game"/>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import GameCard from '~/components/GameCard'
+
+export default {
+  name: 'Home',
+
+  components: {
+    GameCard
+  },
+
+  async asyncData() {
+    const { data: games } = await axios.get('https://shamhawks.now.sh/api')
+    return {
+      games
+    }
+  }
+}
+</script>
